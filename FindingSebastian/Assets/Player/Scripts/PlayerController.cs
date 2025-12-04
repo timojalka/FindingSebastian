6271da80487136b1f7e5cedda3771aa769d518a0
Timo Jalkanen <timojalka@gmail.com>
2025-12-04 21:33:50 +0200

    Acceleration, moveSpeed and drag

--- FindingSebastian/Assets/Player/Scripts/PlayerController.cs
+++ FindingSebastian/Assets/Player/Scripts/PlayerController.cs
@@ -1,16 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] CharacterController _characterController;
+    [SerializeField] Camera _playerCamera;
+
+    public PlayerLocomotionInput _playerLocomotionInput { get; private set; }
+
+    public float moveAcceleration = 0.25f;
+    public float MoveSpeed = 5f;
+    public float drag = 0.1f;
+
+    private void Awake()
     {
-        
+        _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //Movement
+    private void Update()
     {
-        
+        Vector3 cameraForwardXZ = new Vector3(_playerCamera.transform.forward.x, 0f, _playerCamera.transform.forward.z).normalized;
+        Vector3 cameraRightXZ = new Vector3(_playerCamera.transform.right.x, 0f, _playerCamera.transform.right.z).normalized;
+        Vector3 moveDirection = cameraRightXZ * _playerLocomotionInput.MovementInput.x + cameraForwardXZ * _playerLocomotionInput.MovementInput.y;
+
+        Vector3 movementDelta = moveDirection * moveAcceleration * Time.deltaTime;
+        Vector3 newVelocity = _characterController.velocity + movementDelta;
+
+        // Apply drag
+        Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
+        newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
+        newVelocity = Vector3.ClampMagnitude(newVelocity, MoveSpeed);
+
+        // Move character
+        _characterController.Move(newVelocity * Time.deltaTime);
     }
 }
+
--- FindingSebastian/Assets/Player/Scripts/PlayerLocomotionInput.cs
+++ FindingSebastian/Assets/Player/Scripts/PlayerLocomotionInput.cs
@@ -1,16 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerLocomotionInput : MonoBehaviour
+public class PlayerLocomotionInput : MonoBehaviour, PlayerControls.IGameplayActions
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public PlayerControls PlayerControls { get; private set; }
+    public Vector2 MovementInput {  get; private set; }
+
+    private void OnEnable()
     {
-        
+        PlayerControls = new PlayerControls();
+        PlayerControls.Enable();
+
+        PlayerControls.Gameplay.Enable();
+        PlayerControls.Gameplay.SetCallbacks(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        PlayerControls.Gameplay.Disable();
+        PlayerControls.Gameplay.RemoveCallbacks(this);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    { 
+        MovementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
     }
 }
